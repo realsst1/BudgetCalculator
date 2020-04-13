03c04670550dfdfb01e75416b52665b07ac4f6a0
Shreyesh <shreyesh2008@gmail.com>
2020-04-13 18:44:31 +0530

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { MainComponentComponent } from './components/main-component/main-component.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,component:MainComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './components/main-component/main-component.component';
+import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './components/budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
